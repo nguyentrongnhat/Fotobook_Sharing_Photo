3cb0a26955c89c7bf1fd14c3fba548dab15e4b4b
nguyentrongnhat <nguyentrongnhat111999@gmail.com>
2020-08-07 16:34:52 +0700

    add modified album model dependent: destroy

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,7 +3,7 @@ class Album < ApplicationRecord
 	#validates :decription, length: { maximum: 1024 }, allow_blank: true
 	#validates :source, presence: true
 	belongs_to :user
-	has_many :aps
+	has_many :aps, dependent: :destroy
 	has_many :photos, through: :aps
 	has_many :reacts, as: :reactable, dependent: :destroy
 end

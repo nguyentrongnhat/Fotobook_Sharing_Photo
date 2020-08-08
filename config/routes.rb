19d8a73d4b4e15986c075205d6ec1212eb1c108f
nguyentrongnhat <nguyentrongnhat111999@gmail.com>
2020-08-08 18:22:33 +0700

    first config routes for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, :photos, :album
+  get "/login/", to: "login#index"
+  post "/login/", to: "login#handleLogin"
+  get "/logout/", to: "logout#logout"
+  get "/feeds/", to: "feeds#index"
+  get "/discovery", to: "discovery#index"
+  get "/profile/photos", to: "profile#phtos"
+  get "/profile/albums", to: "profile#albums"
+  get "/profile/followings", to: "profile#followings"
+  get "/profile/followers", to: "profile#followers"
+
+  namespace :admin do
+    resources :photos, :albums, :users
+  end
 end
